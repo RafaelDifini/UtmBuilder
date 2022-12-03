eb8b18bc8b734d9ae953f538d7b4b260008ad4c4
Rafael Difini <rafaeldifini@live.com>
2022-12-03 14:03:34 -0300

    First Commit

---
+++ UtmBuilder.Mobile/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UtmBuilder.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPageViewModel ViewModel { get; set; } = new();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        this.BindingContext = ViewModel;
+    }
+}
